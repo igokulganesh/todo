36075d7c917945ce967d26a3f8aab3c939ac3283
Gokul Ganesh I <igokulganesh@gmail.com>
2024-07-11 22:39:50 +0530

    Added Frontend Todo App

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge">
+          Frontend Mentor
+        </a>
+        .
+      </p>
+      <p>
+        Coded by <a href="http://cyishere.dev">CY</a>.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
